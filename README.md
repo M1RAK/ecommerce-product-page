8cf0cf88d1b1ac6fc20eb80e606fac99dc0c7f04
ARA <M1RAK>
2023-03-03 08:30:03 +0100

    Completed!

---
+++ README.md
@@ -0,0 +1,61 @@
+# Frontend Mentor - E-commerce product page solution
+
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+
+### Screenshot
+
+![My Solution](./screenshot.jpg)
+
+### Links
+- Solution URL: [Source Code](https://github.com/M1RAK/ecommerce-product-page)
+<!-- - Live Site URL: [Live site](https://quotables.netlify.app) -->
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Vanilla Js
+
+
+
+<!-- ## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
+- Twitter - [@yourusername](https://www.twitter.com/yourusername)
+
+**Note: Delete this note and add/remove/edit lines above based on what links you'd like to share.**
+
+## Acknowledgments
+
+This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
+
+**Note: Delete this note and edit this section's content as necessary. If you completed this challenge by yourself, feel free to delete this section entirely.** -->
---
+++ js/index.js
@@ -0,0 +1,74 @@
+// Mobile menu
+const menuToggler = document.querySelector(".toggle")
+const linksContainer = document.querySelector(".links-container")
+const listLinks = document.querySelectorAll(".list-link")
+
+function toggle() {
+  menuToggler.classList.toggle("active")
+  linksContainer.classList.toggle("active")
+}
+
+menuToggler.addEventListener("click", toggle)
+
+listLinks.forEach((link) => {
+  link.addEventListener("click", toggle)
+})
+
+// Lightbox toggle
+const heroImg = document.querySelector(".hero-image")
+const overLay = document.querySelector("#overlay")
+const lightboxContainer = document.querySelector(".lightbox-container")
+const lightboxClose = document.querySelector(".lightbox-close")
+
+// Close Functionality makes image-grid work by removing ".active"
+const toggleLightbox = () => {
+  overLay.classList.toggle("active")
+  lightboxContainer.classList.toggle("active")
+}
+
+heroImg.addEventListener("click", toggleLightbox)
+lightboxClose.addEventListener("click", toggleLightbox)
+
+// Image Grid
+
+let slideIndex = 1
+slideShow(slideIndex)
+
+// Next/previous controls
+function toggleSlide(n) {
+  slideShow((slideIndex += n))
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  slideShow((slideIndex = n))
+}
+
+function slideShow(n) {
+  const slides = document.querySelector(".hero-image")
+  const lightboxSlide = document.querySelector(".hero-lg")
+  const smImages = document.querySelectorAll(".lg")
+
+  
+  if (n === 0) {
+    slideIndex = 4
+  }
+  if (n === 5) {
+    slideIndex = 1
+  }
+
+  lightboxSlide.innerHTML = `
+   <img src="./images/image-product-${slideIndex}.jpg" class="hero" alt="hero-image" />`
+
+  // Checks if lightbox Container is active; disables image toggle on main hero
+  slides.innerHTML = `
+   <img src="./images/image-product-${
+     lightboxContainer.classList.contains("active") ? 1 : slideIndex
+   }.jpg" alt="hero-image" />`
+
+  smImages.forEach((img) => img.classList.remove("active"))
+
+  smImages[slideIndex - 1].classList.add("active")
+
+  // Because [] are zero indexed; Element 1 - 1 = 1
+}
